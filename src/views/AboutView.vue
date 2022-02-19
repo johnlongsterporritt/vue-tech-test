58305533ce3fd7c4d9c15187ca829a809b30d1b0
johnlongsterporritt <johnlporritt@gmail.com>
2022-02-19 13:00:20 +0000

    Task 3

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,44 @@
 <template>
     <div class="about">
         <div class="hero">
-            <h1>About us</h1>
-            <h3>Lorem ipsum dolar sit amet</h3>
+            <div class="hero-info">
+              <h1>About Us</h1>
+              <h3>Lorem ipsum dolar sit amet</h3>
+              <a class="btn btn-primary" href="#">Call to action</a>
+            </div>
         </div>
 
         <div class="body">
-            <h2>Lorem Ipsum</h2>
-            <p>
-                Quisque pulvinar id urna eu iaculis. Mauris finibus congue lacus
-                at pretium. Phasellus fermentum nibh eget eros placerat, ut
-                vestibulum turpis consectetur. Duis sit amet tellus at mauris
-                tincidunt elementum sit amet eu massa. Nulla lacinia eu tortor
-                vitae cursus. Proin et molestie diam. Vestibulum eget suscipit
-                ante. Vestibulum egestas at elit eu elementum. Curabitur
-                vehicula dolor eu dictum ullamcorper. Interdum et malesuada
-                fames ac ante ipsum primis in faucibus. Nulla sit amet lorem
-                leo. Proin ultricies feugiat augue, in iaculis lectus porttitor
-                sed. Nulla eget lectus vitae orci interdum commodo. Nullam
-                fringilla ultricies ultrices. Sed at odio tortor. Pellentesque
-                ut congue justo. Donec vel viverra mauris. Mauris eu augue
-                dignissim, tincidunt nisi nec, malesuada neque. Donec facilisis
-                quam non cursus finibus. Pellentesque et sapien non ipsum
-                pharetra volutpat quis id felis. Sed mattis varius est nec
-                tempus. Fusce commodo sed lectus vel ornare. Vivamus tincidunt
-                est lorem, eget feugiat libero ultrices eleifend. Aenean dapibus
-                libero tellus, quis elementum leo mollis id. Morbi id magna
-                porta, scelerisque nisi eget, hendrerit tellus.
-            </p>
-            <a href="">Learn More</a>
+            <h2 class="mb-4">Lorem Ipsum</h2>
+            <img src="@/assets/about/image1.png" class="float-image img-handshake">
+            <img src="@/assets/about/image2.png" class="float-image img-talk">
+
+            <div class="text-contain">
+                <p>
+                    Quisque pulvinar id urna eu iaculis. Mauris finibus congue lacus
+                    at pretium. Phasellus fermentum nibh eget eros placerat, ut
+                    vestibulum turpis consectetur. Duis sit amet tellus at mauris
+                    tincidunt elementum sit amet eu massa. Nulla lacinia eu tortor
+                    vitae cursus. Proin et molestie diam. Vestibulum eget suscipit
+                    ante. Vestibulum egestas at elit eu elementum. Curabitur
+                    vehicula dolor eu dictum ullamcorper.
+                </p>
+                <p class="mb-5">
+                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sit amet lorem
+                    leo. Proin ultricies feugiat augue, in iaculis lectus porttitor
+                    sed. Nulla eget lectus vitae orci interdum commodo. Nullam
+                    fringilla ultricies ultrices. Sed at odio tortor. Pellentesque
+                    ut congue justo. Donec vel viverra mauris. Mauris eu augue
+                    dignissim, tincidunt nisi nec, malesuada neque. Donec facilisis
+                    quam non cursus finibus. Pellentesque et sapien non ipsum
+                    pharetra volutpat quis id felis. Sed mattis varius est nec
+                    tempus. Fusce commodo sed lectus vel ornare. Vivamus tincidunt
+                    est lorem, eget feugiat libero ultrices eleifend. Aenean dapibus
+                    libero tellus, quis elementum leo mollis id. Morbi id magna
+                    porta, scelerisque nisi eget, hendrerit tellus.
+                </p>
+            </div>
+            <a class="btn btn-primary" href="#">Learn More</a>
         </div>
     </div>
 </template>
