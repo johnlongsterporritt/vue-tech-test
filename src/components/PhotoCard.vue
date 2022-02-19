4590441eae6fb0c270549a7a860c82c2fb3bac07
johnlongsterporritt <johnlporritt@gmail.com>
2022-02-19 11:51:09 +0000

    Task 2

--- src/components/PhotoCard.vue
+++ src/components/PhotoCard.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="col-12 col-md-6 col-lg-4 photo-card-container">
-      <div class="photo-card">
-          <h3>
-              {{ photo.photographer }}
-          </h3>
+        <div class="photo-card">
+            <a :href="photo.url" target="_blank" class="background-image" :title="photo.alt">
+              <img :src="photo.src.large" :alt="photo.alt" class="img-fluid">
+            </a>
 
-          <p>
-              {{ photo.alt }}
-          </p>
+              <h3>
+                  {{ photo.photographer }}
+              </h3>
 
-          <a href="">
-              View Original
-          </a>
-      </div>
+              <p>
+                  {{ photo.alt }}
+              </p>
+
+              <a :href="photo.url" target="_blank">
+                  View Original
+              </a>
+        </div>
     </div>
 </template>
 
